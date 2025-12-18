ebcb5a66488ac9e42f2c4173cada6e6c421dadd6
AriciaLima <ariciafariasl@gmail.com>
2025-12-18 16:38:20 +0000

    Initial Laravel project setup

---
+++ laravel/database/migrations/2025_12_18_162724_create_gifts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+{
+    Schema::create('gifts', function (Blueprint $table) {
+        $table->id();
+        $table->string('name');
+        $table->decimal('expected_value', 8, 2);
+        $table->decimal('spent_value', 8, 2)->nullable();
+        $table->foreignId('user_id')
+              ->constrained()
+              ->onDelete('cascade');
+        $table->timestamps();
+    });
+}
+
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+{
+    Schema::dropIfExists('gifts');
+}
+};
